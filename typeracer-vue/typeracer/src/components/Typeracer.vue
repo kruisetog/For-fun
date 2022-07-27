d5666918646d1ee4178a47a4b00b6e777cc75e4c
kruisetog <91522301+kruisetog@users.noreply.github.com>
2022-07-28 03:37:28 +0800

    Initial commit

---
+++ typeracer-vue/typeracer/src/components/Typeracer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Typeracer</h3>
+    <div>
+        <p>This is the quote to type out: {{ quote }}</p>
+        <textarea v-model="userInput"></textarea>
+    </div>
+  </div>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+export default defineComponent({
+  name: 'Typeracer',
+  components: {
+    
+  },
+  setup(){
+    const userInput = ref('')
+    const quote = 'Look forward to today!'
+    return{
+        userInput,
+        quote
+    }
+  }
+
+})
+</script>
+
+<style>
+
+</style>
